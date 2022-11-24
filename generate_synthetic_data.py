231a97d104c177b0ac36f5869a5b6c91ffc0f108
André Svensson <an3252sv-s@student.lu.se>
2022-11-24 16:32:22 +0100

    fix: instruments are no longer pixelated, but the code is quite messy

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -8,7 +8,7 @@ import random
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
 
-from PIL import Image
+from PIL import Image, ImageEnhance
 import random as rand
 import numpy
 import time
@@ -22,11 +22,52 @@ def get_args():
     return parser.parse_args()
 
 
+
+def show_img(img):
+    return
+    img_h, img_w = img.shape[0], img.shape[1]
+    print(f"image w: {img_w} h: {img_h}")
+    win_name = "window"
+    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(win_name, img)
+
+    max_size = 1080
+    width_ratio, height_ratio = max_size/img_w, max_size/img_h
+    scale_factor = min(1, width_ratio, height_ratio)
+    window_w, window_h = int(scale_factor*img_w), int(scale_factor*img_h)
+    print(f"window w: {window_w} h: {window_h}\n")
+
+    cv2.resizeWindow(win_name, 3840, 2160) #resize must come after imshow for it to work properly
+    cv2.waitKey(0)
+
+def show_img_1(img):
+    img_h, img_w = img.shape[0], img.shape[1]
+    print(f"image w: {img_w} h: {img_h}")
+    win_name = "window"
+    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(win_name, img)
+
+    max_size = 1080
+    width_ratio, height_ratio = max_size/img_w, max_size/img_h
+    scale_factor = min(1, width_ratio, height_ratio)
+    window_w, window_h = int(scale_factor*img_w), int(scale_factor*img_h)
+    print(f"window w: {window_w} h: {window_h}\n")
+
+    cv2.resizeWindow(win_name, 1920, 1080) #resize must come after imshow for it to work properly
+    cv2.waitKey(0)
+
+#assumes input mask values are 0 and background values are 1
+def show_mask(mask):
+    mask = mask.copy()
+    mask[mask == 0] = 255
+    mask[mask == 1] = 0
+    show_img(mask)
+
+
+
 '''
 Replace background of image with average color of cv2_img pixels that contain the mask.
 '''
-
-
 def replace_background_with_average_color(cv2_img, mask):
     for i in range(3):
         cv2_img[mask == 1, i] = np.mean(cv2_img[mask == 0, i])
@@ -61,11 +102,10 @@ def get_img_and_mask(img_path, color):
     mask = skimage.exposure.rescale_intensity(blur, in_range=(
         127.5, 255), out_range=(0, 255)).astype(np.uint8)
 
-    mask = cv2.cvtColor(mask, cv2.COLOR_RGB2BGR)
-    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
-    mask_b = mask[:, :, 0] == 0
-    mask = mask_b.astype(np.uint8)
     cv2_img = replace_background_with_average_color(cv2_img, mask)
+
+    show_img(cv2_img)
+    show_img(mask)
 
     return cv2_img, mask
 
@@ -78,7 +118,7 @@ Performs image augmentation and resizing
 def resize_transform_obj(img, mask, longest_min, longest_max, transforms=False):
 
     transform = Al.Compose([
-        Al.RandomBrightnessContrast(),
+        Al.RandomBrightnessContrast(brightness_limit=(-0.5,-0.1), p=0.9),
         Al.RandomGamma(),
         Al.augmentations.transforms.Downscale(
             scale_min=0.30, scale_max=0.75, interpolation=cv2.INTER_LINEAR, always_apply=False, p=0.5)
@@ -100,9 +140,38 @@ def resize_transform_obj(img, mask, longest_min, longest_max, transforms=False):
     img = crop_transform["image"]
     mask = flip_transform["mask"]
     mask = crop_transform["mask"]
+    #print("clip and flip mask")
+    #show_img(mask)
     transformed = transform(image=img, bboxes=[])
     transformed_image = transformed['image']
     h, w = transformed_image.shape[0], transformed_image.shape[1]
+    longest, shortest = max(h, w), min(h, w)
+    longest_new = np.random.randint(longest_min, longest_max)
+    shortest_new = int(shortest * (longest_new / longest))
+
+    '''if h > w:
+        h_new, w_new = longest_new, shortest_new
+    else:
+        h_new, w_new = shortest_new, longest_new
+    transform_resize = Al.Resize(
+        h_new, w_new, interpolation= cv2.INTER_LANCZOS4, always_apply=False, p=1)'''
+
+    #transformed_resized = transform_resize(
+    #    image=transformed_image.copy(), mask=mask.copy())
+    #img_t = transformed_resized["image"]
+    #mask_t = transformed_resized["mask"]
+
+
+
+
+
+    pil_img = Image.fromarray(transformed_image.copy())
+    pil_mask = Image.fromarray(mask)
+    angle = rand.randint(0, 360)
+    pil_img = pil_img.rotate(angle, expand=True)
+    pil_mask = pil_mask.rotate(angle, expand=True)
+
+    h, w = pil_img.height, pil_img.width
     longest, shortest = max(h, w), min(h, w)
     longest_new = np.random.randint(longest_min, longest_max)
     shortest_new = int(shortest * (longest_new / longest))
@@ -111,15 +180,22 @@ def resize_transform_obj(img, mask, longest_min, longest_max, transforms=False):
         h_new, w_new = longest_new, shortest_new
     else:
         h_new, w_new = shortest_new, longest_new
-    transform_resize = Al.Resize(
-        h_new, w_new, interpolation=1, always_apply=False, p=1)
 
-    transformed_resized = transform_resize(
-        image=transformed_image, mask=mask)
-    img_t = transformed_resized["image"]
-    mask_t = transformed_resized["mask"]
+    #show_img(np.array(pil_img))
+    #show_img(mask_t)
+    #print("PIL mask")
+    #show_img(np.array(pil_mask))
 
-    m = mask_t.copy()
+    pil_img_cp = pil_img.resize((w_new ,h_new),  Image.Resampling.LANCZOS)
+    pil_mask_cp = pil_mask.resize((w_new ,h_new),  Image.Resampling.LANCZOS)
+    cv_img = np.array(pil_img_cp)
+    cv_mask = np.array(pil_mask_cp)
+    #show_img(img_t)
+    #show_img(cv_img)
+    #show_img(mask_t)
+    #print("PIL mask")
+    #show_img(cv_mask)
+
     #cv2.imshow("hej", m.astype(float))
     # cv2.waitKey(0)
 
@@ -127,25 +203,29 @@ def resize_transform_obj(img, mask, longest_min, longest_max, transforms=False):
     # -------------------------------------------------------------------------------------------------
 
     # changes the mask value from 0 because the rotated image will generate pixelvalues = 0
-    mask_t[mask_t == 0] = 100
+    #mask_t[mask_t == 0] = 100
 
-    pil_img = Image.fromarray(img_t)
-    pil_mask = Image.fromarray(mask_t)
-    angle = rand.randint(0, 360)
-    pil_img = pil_img.rotate(angle, expand=True)
-    pil_mask = pil_mask.rotate(angle, expand=True)
+    #pil_img = Image.fromarray(img_t)
+    #pil_mask = Image.fromarray(mask_t)
+    #angle = rand.randint(0, 360)
+    #pil_img = pil_img.rotate(angle, expand=True)
+    #pil_mask = pil_mask.rotate(angle, expand=True)
 
-    img_t = np.array(pil_img)
-    mask_t = np.array(pil_mask)
+    img_t = cv_img
+    mask_t = cv_mask
     # resets the original mask values to 0 and changes the newly added pixels to 1.
-    mask_t[mask_t == 0] = 1
-    mask_t[mask_t == 100] = 0
+    #mask_t[mask_t == 0] = 1
+    #mask_t[mask_t == 100] = 0
     # -------------------------------------------------------------------------------------------------
 
     if transforms:
         transformed = transforms(image=img_t, mask=mask_t)
         img_t = transformed["image"]
         mask_t = transformed["mask"]
+
+    #show_img(img_t)
+    #print("resized and rotated mask")
+    #show_img(mask_t)
 
     return img_t, mask_t
 
@@ -155,7 +235,152 @@ Adds an object to the background image
 '''
 
 
+
+#With PIL
 def add_obj(img_comp, mask_comp, img, mask, x, y, idx, is_noise=False):
+    '''
+    img_comp - composition of objects
+    mask_comp - composition of objects` masks
+    img - image of object
+    mask - binary mask of object
+    x, y - coordinates where center of img is placed
+    Function returns img_comp in CV2 RGB format + mask_comp
+    '''
+    h_comp, w_comp = img_comp.height, img_comp.width
+
+    h, w = img.shape[0], img.shape[1]
+    '''if (random.randint(0, 100) in range(0, int(100*0.5))):
+        img = shade(img,random.uniform(0.1, 0.7))
+    '''
+    x = x - int(w/2)
+    y = y - int(h/2)
+
+    #creates a binary mask
+    #temp_mask = cv2.cvtColor(mask.copy(), cv2.COLOR_RGB2BGR)
+    #temp_mask = cv2.cvtColor(temp_mask, cv2.COLOR_BGR2RGB)
+    mask_b = ~(mask == 0)
+    #print(mask_b[0])
+    #print(mask[100])
+    #show_img_1(mask)
+    mm = mask_b.astype(np.uint8)
+    mm[mm == 1] = 255
+    #show_img_1(mm)
+    mask_rgb_b = np.stack([mask_b, mask_b, mask_b], axis=2)
+    mask_added = []
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+    img[:,:,3] = mask
+    img = Image.fromarray(img)
+
+    if x >= 0 and y >= 0:
+
+        # h_part - part of the image which gets into the frame of img_comp along y-axis
+        
+        h_part = h - max(0, y+h-h_comp)
+        # w_part - part of the image which gets into the frame of img_comp along x-axis
+        w_part = w - max(0, x+w-w_comp)
+
+        #print(f"y:{y}, h:{h}, h_comp:{h_comp}, h_part:{h_part}")
+        #print(f"img_w:{w}, img_h:{h}, paste_w:{w_part}, paste_h:{h_part}")
+        #print()
+
+        #img_comp[y:y+h_part, x:x+w_part, :] = img_comp[y:y+h_part, x:x+w_part, :] * \
+        #    ~mask_rgb_b[0:h_part, 0:w_part, :] + \
+        #    (img * mask_rgb_b)[0:h_part, 0:w_part, :]
+        
+        #img_comp.show()
+        #import time
+        #time.sleep(3)
+        #img.show()
+        #time.sleep(3)
+        #print(f"img_comp {img_comp.width} {img_comp.height} img {img.width} {img.height}")
+        #print(f"location: {(x, y, x+w_part, y+h_part)}")
+        #print("\n x and y > 0 ")
+        #print(f" paste into {(x, y, x+w_part, y+h_part)}\n")
+        #print(f"background size: {img_comp.size} {img_comp.mode}, object size: {img.size} {img.mode}")
+        cropped_obj = img.crop((0,0, w_part,h_part))
+        img_comp.paste(cropped_obj, (x, y, x+w_part, y+h_part), cropped_obj)
+        #cv2.imshow("hej", np.array(img_comp))
+        #cv2.resizeWindow("hej", 3840, 2160)
+        #cv2.waitKey(0)
+        if (not is_noise):
+            mask_comp[y:y+h_part, x:x+w_part] = mask_comp[y:y+h_part, x:x+w_part] * \
+                ~mask_b[0:h_part, 0:w_part] + \
+                (idx * mask_b)[0:h_part, 0:w_part]
+            mask_added = (~mask_b[0:h_part, 0:w_part]).astype(np.uint8)
+
+    elif x < 0 and y < 0:
+
+        h_part = h + y
+        w_part = w + x
+
+        #print("\n x and y < 0 ")
+        #print(f" paste into {(0, 0, w_part, h_part)}\n")
+
+        cropped_obj = img.crop((w-w_part,h-h_part, w,h))
+        img_comp.paste(cropped_obj, (0, 0, w_part, h_part), cropped_obj)
+
+        #img_comp[0:0+h_part, 0:0+w_part, :] = img_comp[0:0+h_part, 0:0+w_part, :] * \
+        #    ~mask_rgb_b[h-h_part:h, w-w_part:w, :] + \
+        #    (img * mask_rgb_b)[h-h_part:h, w-w_part:w, :]
+        if (not is_noise):
+
+            mask_comp[0:0+h_part, 0:0+w_part] = mask_comp[0:0+h_part, 0:0+w_part] * \
+                ~mask_b[h-h_part:h, w-w_part:w] + \
+                (idx * mask_b)[h-h_part:h, w-w_part:w]
+            mask_added = (~mask_b[h-h_part:h, w-w_part:w]).astype(np.uint8)
+
+    elif x < 0 and y >= 0:
+
+        h_part = h - max(0, y+h-h_comp)
+        w_part = w + x
+
+        #print("\n x < 0 and y > 0 ")
+        #print(f" paste into {(0, y, w_part, y+h_part)}\n")
+
+        cropped_obj = img.crop((w-w_part,0, w,h_part))
+        img_comp.paste(cropped_obj, (0, y, w_part, y+h_part), cropped_obj)
+
+        #img_comp[y:y+h_part, 0:0+w_part, :] = img_comp[y:y+h_part, 0:0+w_part, :] * \
+        #    ~mask_rgb_b[0:h_part, w-w_part:w, :] + \
+        #    (img * mask_rgb_b)[0:h_part, w-w_part:w, :]
+        if (not is_noise):
+
+            mask_comp[y:y+h_part, 0:0+w_part] = mask_comp[y:y+h_part, 0:0+w_part] * \
+                ~mask_b[0:h_part, w-w_part:w] + \
+                (idx * mask_b)[0:h_part, w-w_part:w]
+            mask_added = (~mask_b[0:h_part, w-w_part:w]).astype(np.uint8)
+
+    elif x >= 0 and y < 0:
+
+        h_part = h + y
+        w_part = w - max(0, x+w-w_comp)
+
+        #print("\n x > 0 and y < 0 ")
+        #print(f" paste into {(x, 0, x+w_part, h_part)}\n")
+
+        cropped_obj = img.crop((0,h-h_part, w_part,h))
+        img_comp.paste(cropped_obj, (x, 0, x+w_part, h_part), cropped_obj)
+
+        #img_comp[0:0+h_part, x:x+w_part, :] = img_comp[0:0+h_part, x:x+w_part, :] * \
+        #    ~mask_rgb_b[h-h_part:h, 0:w_part, :] + \
+        #    (img * mask_rgb_b)[h-h_part:h, 0:w_part, :]
+        if (not is_noise):
+
+            mask_comp[0:0+h_part, x:x+w_part] = mask_comp[0:0+h_part, x:x+w_part] * \
+                ~mask_b[h-h_part:h, 0:w_part] + \
+                (idx * mask_b)[h-h_part:h, 0:w_part]
+            mask_added = (~mask_b[h-h_part:h, 0:w_part]).astype(np.uint8)
+
+    #show_img(np.array(img_comp))
+
+    #show_img_1(mask_comp)
+    #print(mask_added[0])
+
+    return img_comp, mask_comp, mask_added
+
+
+def add_obj_old(img_comp, mask_comp, img, mask,  x, y, idx, is_noise=False):
     '''
     img_comp - composition of objects
     mask_comp - composition of objects` masks
@@ -178,9 +403,14 @@ def add_obj(img_comp, mask_comp, img, mask, x, y, idx, is_noise=False):
     if x >= 0 and y >= 0:
 
         # h_part - part of the image which gets into the frame of img_comp along y-axis
+        
         h_part = h - max(0, y+h-h_comp)
         # w_part - part of the image which gets into the frame of img_comp along x-axis
         w_part = w - max(0, x+w-w_comp)
+
+        #print(f"y:{y}, h:{h}, h_comp:{h_comp}, h_part:{h_part}")
+        #print(f"img_w:{w}, img_h:{h}, paste_w:{w_part}, paste_h:{h_part}")
+        #print()
 
         img_comp[y:y+h_part, x:x+w_part, :] = img_comp[y:y+h_part, x:x+w_part, :] * \
             ~mask_rgb_b[0:h_part, 0:w_part, :] + \
@@ -236,6 +466,8 @@ def add_obj(img_comp, mask_comp, img, mask, x, y, idx, is_noise=False):
                 (idx * mask_b)[h-h_part:h, 0:w_part]
             mask_added = mask[h-h_part:h, 0:w_part]
 
+    show_img(img_comp)
+
     return img_comp, mask_comp, mask_added
 
 
@@ -244,10 +476,18 @@ def check_areas(mask_comp, obj_areas, labels_comp=[], overlap_degree=0.3, i=10):
 
     # Här hämtas listan av vad som syns i respektive mask. De hämtas genom att pixlarna skiljer lite på varje mask.
     # Varje mask skiljer en pixel och mask_comp innehåller all info, alltså hur alla masks ligger på varandra.
+    #print("inside check area")
+    #print(f"obj_ids:{obj_ids}")
     masks = mask_comp == obj_ids[:, None, None]
+    #print("\n\n")
+    #print(masks.shape)
+    #print("\n\n")
+    
 
     ok = True
     for idx, mask in enumerate(masks):
+        #print(mask.shape)
+        #show_img_1(mask)
         if (obj_areas[idx][0] != 0):
             if np.count_nonzero(mask) / obj_areas[idx][0] < 1 - overlap_degree:
                 ok = False
@@ -268,7 +508,9 @@ def shade(imag, percent):
 Positions objects on a background image, they may overlap to a certain degree.
 '''
 
-
+'''
+longest_min and longesth_max are the minumum and maximum
+'''
 def create_composition(obj_dict, img_comp_bg,  longest_min, longest_max, noise_dict,
                        max_objs=5,
                        overlap_degree=0.2,
@@ -288,6 +530,20 @@ def create_composition(obj_dict, img_comp_bg,  longest_min, longest_max, noise_d
     i = 1
     p = 1
     random_obj = 0
+
+    '''
+    1. We convert backround image to PIL format
+    2. blurry mask is generated in 'get_img_and_mask'
+    3. 'resize_transform_obj' transforms image and the blurry mask
+    4. in add_obj, a binary mask is created from the blurry mask
+    5. binary mask is used to check for overlap with other objects
+    and the blurry mask is used to paste the object on the background image
+
+    '''
+
+    img_comp = Image.fromarray(cv2.cvtColor(img_comp, cv2.COLOR_BGR2RGB))
+
+
     for _ in range(1, num_objs):
         obj_idx = np.random.randint(len(obj_dict)) + 1
         noise_idx = np.random.randint(len(noise_dict)) + 1
@@ -300,7 +556,7 @@ def create_composition(obj_dict, img_comp_bg,  longest_min, longest_max, noise_d
             img_path = obj_dict[obj_idx]['images'][idx]
             ##If it is saved, take mask and image directly.
             if img_path in saved_images_and_masks:
-                img,mask = saved_images_and_masks[img_path]
+                img,mask, = saved_images_and_masks[img_path]
             else:
                 img, mask = get_img_and_mask(
                     img_path, obj_dict[obj_idx]["background_color"])
@@ -427,7 +683,7 @@ def create_composition(obj_dict, img_comp_bg,  longest_min, longest_max, noise_d
                     p += 1
                     img_comp, mask_comp = img_comp_prev.copy(), mask_comp_prev.copy()
 
-    return img_comp, mask_comp, labels_comp,saved_images_and_masks
+    return cv2.cvtColor(np.array(img_comp), cv2.COLOR_BGR2RGB), mask_comp, labels_comp,saved_images_and_masks
 
 
 def initiate_annotation(final_image_path, img_width, img_height):
@@ -518,9 +774,10 @@ def generate_images_and_xml(INPUT_LIST, BACKGROUND_LIST, NOISE_LIST, NUMBER_OF_I
                                                                          overlap_degree=OVERLAP_DEGREE,
                                                                          max_attempts_per_obj=10,saved_images_and_masks=saved_images_and_masks)
 
+        #show_img_1(mask_comp)
         img_comp_bboxes = img_comp.copy()
         new_output_name = os.path.join(OUTPUT_FOLDER, os.path.basename(
-            image_background_path))[:-4]+"gen_"+str(x)+".png"
+            image_background_path))[:-4]+"gen_"+str(x)+".jpg"
         cv2.imwrite(new_output_name, img_comp_bboxes)
 
         # save annotations
